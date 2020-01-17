7931f0b1c641879f9c8f70a0a7a9ff0086ec08d2
Alec Dye <alecdye@gmail.com>
2020-01-17 09:42:42 -0800

    building card func

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,11 @@
 //
 // 4. Create a card for each of the articles and add the card to the DOM.
 
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.log('Something broke', error)
+    });
+
